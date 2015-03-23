e4936e82e235ffcc18f278f72ad072dfae980a8e
Grace Mekarski and Tyler Brown <elementalplug@gmail.com>
2015-03-23 12:28:47 -0700

    add crud to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have successfully updated #{@user.user_name}!"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "Account destroyed!"
+    redirect_to '/'
+  end
+
+
 
 private
   def user_params
     params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
   end
-  
+
 end
